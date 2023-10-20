d72f37da5ce62b24416b2e1f2a696ea860f58310
Nguyen Sam <sam.np@geekup.vn>
2023-10-20 23:52:30 +0700

    update: delete redundant code

--- src/api/services/addressService.js
+++ src/api/services/addressService.js
@@ -3,7 +3,6 @@ import { getAddressData } from "../axios";
 export const getListDataAddress = async () => {
     try {
         const data = await getAddressData(); // Sử dụng hàm getAddressData để lấy dữ liệu
-        console.log('ưdqfiuqwfiuhqfwi: ', data);
         return data; // Trả về dữ liệu từ hàm getAddressData
     } catch (error) {
         console.error('Lỗi khi lấy dữ liệu danh sách địa chỉ:', error);
